8e3fe03352892deefa351596bab7d2d10d2ecd43
Ömer Faruk CAVLAK <omerfarukcavlak@gmail.com>
2024-11-13 18:33:48 +0300

    Expected Exception

---
+++ ExpectedException/ShoppingCart.Tests/CartTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Tests
+{
+    [TestClass]
+    public class CartTests
+    {
+        private static CartItem _cartItem;
+        private static CartManager _cartManager;
+
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _cartManager = new CartManager();
+            _cartItem = new CartItem
+            {
+                Product = new Product
+                {
+                    ProductId = 1,
+                    ProductName = "Laptop",
+                    UnitPrice = 25000
+                },
+                Quantity = 1
+
+            };
+            _cartManager.Add(_cartItem);
+        }
+
+        [ClassCleanup]
+        public static void TestCleanup()
+        {
+            _cartManager.Clear();
+        }
+
+        [TestMethod]
+        public void ShouldIncreaseTotalProductCountAndDistinctItemCount_WhenSecondProductIsAdded()
+        {
+            int totalQuantity = _cartManager.TotalQuantity;
+            int totalItems = _cartManager.TotalItems;
+
+            _cartManager.Add(new CartItem
+            {
+                Product = new Product
+                {
+                    ProductId = 2,
+                    ProductName = "Mouse",
+                    UnitPrice = 10
+                },
+                Quantity = 1
+            });
+
+            Assert.AreEqual(totalQuantity + 1, _cartManager.TotalQuantity);
+            Assert.AreEqual(totalItems + 1, _cartManager.TotalItems);
+        }
+
+
+        [TestMethod]
+        //[ExpectedException(typeof(ArgumentException))]
+        [ExpectedException(typeof(Exception),AllowDerivedTypes = true)]
+        public void ShouldThrowError_WhenSameProductIsAddedToCartTwice()
+        {
+            _cartManager.Add(_cartItem);
+        }
+    }
+}
---
+++ ExpectedException/ShoppingCart.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ShoppingCart.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShoppingCart.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("539848a7-79a0-4ef4-80d1-ed9e22276bf7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
