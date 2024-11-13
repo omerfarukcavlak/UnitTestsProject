00dccae1d89bb4e14a5aef3e1ce1d325073cefd4
Ömer Faruk CAVLAK <omerfarukcavlak@gmail.com>
2024-11-13 18:17:35 +0300

    Test Level

---
+++ TestLevel/ShoppingCart.Tests/CartTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Tests
+{
+    [TestClass]
+    public class CartTests
+    {
+        private CartItem _cartItem;
+        private CartManager _cartManager;
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+             _cartManager = new CartManager();
+             _cartItem = new CartItem
+            {
+                Product = new Product
+                {
+                    ProductId = 1,
+                    ProductName = "Laptop",
+                    UnitPrice = 25000
+                },
+                Quantity = 1
+
+            };
+            _cartManager.Add(_cartItem);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _cartManager.Clear();
+        }
+
+
+
+
+        [TestMethod]
+        public void ShouldBeAbleToAddProductToCart()
+        {
+            const int expected = 1;
+
+            var totalItems = _cartManager.TotalItems;
+
+            Assert.AreEqual(expected, totalItems);
+
+        }
+
+
+        [TestMethod]
+        public void ShouldBeAbleToRemoveProductFromCart()
+        {
+            var numberOfItemsInCart = _cartManager.TotalItems;
+            _cartManager.Remove(1);
+            var remainingItemsInCart = _cartManager.TotalItems;
+
+            Assert.AreEqual(numberOfItemsInCart - 1, remainingItemsInCart);
+        }
+
+        [TestMethod]
+        public void ShouldBeAbleToClearTheCart()
+        {
+            _cartManager.Clear();
+
+            Assert.AreEqual(0, _cartManager.TotalQuantity);
+            Assert.AreEqual(0, _cartManager.TotalItems);
+        }
+    }
+}
---
+++ TestLevel/ShoppingCart.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ShoppingCart.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShoppingCart.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f2087c2e-a026-4c0e-966f-518e5e25768e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
