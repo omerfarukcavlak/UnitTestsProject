b1670503722d67cf95955985cfe7a12a1c71f7d2
Ömer Faruk CAVLAK <omerfarukcavlak@gmail.com>
2024-11-13 18:17:54 +0300

    Class Level

---
+++ ClassLevel/ShoppingCart.Tests/CartTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Tests
+{
+    [TestClass]
+    public class CartTests
+    {
+        private static CartItem _cartItem;
+        private static CartManager _cartManager;
+
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            _cartManager = new CartManager();
+            _cartItem = new CartItem
+            {
+                Product = new Product
+                {
+                    ProductId = 1,
+                    ProductName = "Laptop",
+                    UnitPrice = 25000
+                },
+                Quantity = 1
+
+            };
+            _cartManager.Add(_cartItem);
+        }
+
+        [ClassCleanup]
+        public static void TestCleanup()
+        {
+            _cartManager.Clear();
+        }
+
+        [TestMethod]
+        public void ShouldIncreaseTotalProductCountAndDistinctItemCount_WhenSecondProductIsAdded()
+        {
+            int totalQuantity = _cartManager.TotalQuantity;
+            int totalItems = _cartManager.TotalItems;
+
+            _cartManager.Add(new CartItem
+            {
+                Product = new Product
+                {
+                    ProductId = 2,
+                    ProductName = "Mouse",
+                    UnitPrice = 10
+                },
+                Quantity = 1
+            });
+
+            Assert.AreEqual(totalQuantity + 1, _cartManager.TotalQuantity);
+            Assert.AreEqual(totalItems + 1, _cartManager.TotalItems);
+        }
+
+
+        [TestMethod]
+        public void ShouldIncreaseTotalProductCountByOne_WhenExistingProductIsAdded()
+        {
+            int totalQuantity = _cartManager.TotalQuantity;
+            int totalItems = _cartManager.TotalItems;
+
+            _cartManager.Add(_cartItem);
+
+
+            Assert.AreEqual(totalQuantity + 1, _cartManager.TotalQuantity);
+            Assert.AreEqual(totalItems, _cartManager.TotalItems);
+        }
+    }
+}
---
+++ ClassLevel/ShoppingCart/CartManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart
+{
+    public class CartManager
+    {
+        private readonly List<CartItem> _cartItems;
+        public CartManager()
+        {
+            _cartItems = new List<CartItem>();
+        }
+
+        public void Add(CartItem cartItem)
+        {
+            var addedCartItem = _cartItems.SingleOrDefault(t => t.Product.ProductId == cartItem.Product.ProductId);
+
+            if (addedCartItem == null)
+            {
+                _cartItems.Add(cartItem);
+            }
+            else
+            {
+                addedCartItem.Quantity += cartItem.Quantity;
+            }
+
+        }
+        public void Remove(int productId)
+        {
+            var product = _cartItems.FirstOrDefault(t => t.Product.ProductId == productId);
+            _cartItems.Remove(product);
+        }
+
+        public List<CartItem> CartItems
+        {
+            get { return _cartItems; }
+        }
+
+        public void Clear()
+        {
+            _cartItems.Clear();
+        }
+
+        public decimal TotalPrice
+        {
+            get { return _cartItems.Sum(t => t.Quantity * t.Product.UnitPrice); }
+        }
+
+        public int TotalQuantity
+        {
+            get { return _cartItems.Sum(t => t.Quantity); }
+        }
+
+        public int TotalItems
+        {
+            get { return _cartItems.Count; }
+        }
+    }
+}
