bc4363bad79a138230c7763a776fca619d22a6e2
Ömer Faruk CAVLAK <omerfarukcavlak@gmail.com>
2024-11-13 22:43:49 +0300

    Assert Overload

---
+++ Asserts/Asserts.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Asserts.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Asserts.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("56936e8f-a7a5-4329-974a-8cd455d1c068")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Asserts/Asserts.Tests/UnitTest1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Asserts.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            const double enteredValue = 16;
+            const double expected = 4;
+            double actualValue = Math.Sqrt(enteredValue);
+
+            Assert.AreEqual(expected, actualValue, "The square root of {0} should be {1}", enteredValue, expected);
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            double expected = 3.1622;
+            double delta = 0.0001;
+            double actual = Math.Sqrt(10);
+            /*
+             if(|actual - expected|<= delta)
+            {
+            Assert.AreEqual(expected, actual, delta) =true;
+            }
+
+            else
+            {
+            Assert.AreEqual(expected, actual, delta) =false;
+            }
+             */
+            Assert.AreEqual(expected, actual,delta);
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string expected = "MERHABA";
+            string actual = "Merhaba";
+
+            Assert.AreEqual(expected, actual,true);
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            const double notExpected = 0;
+            var actual = Math.Pow(5, 0);
+
+            Assert.AreNotEqual(notExpected, actual);
+
+        }
+    }
+}
