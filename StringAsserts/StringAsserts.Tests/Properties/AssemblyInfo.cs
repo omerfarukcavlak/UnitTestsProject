c07aebc37a949fd684c0e479eec99e5d401949a6
Ömer Faruk CAVLAK <omerfarukcavlak@gmail.com>
2024-11-14 00:28:57 +0300

    String Assert Added

---
+++ StringAsserts/StringAsserts.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringAsserts.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringAsserts.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ddb28ab-306f-41d3-8744-11157026b337")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StringAsserts/StringAsserts.Tests/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text.RegularExpressions;
+
+namespace StringAsserts.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void StringContainsTest()
+        {
+            StringAssert.Contains("Test dünyasından merhaba", "yas");
+        }
+        
+        [TestMethod]
+        public void StringMatchesTest()
+        {
+            StringAssert.Matches("Test dünyasından merhaba", new Regex(@"[a-zA-Z]") );
+            StringAssert.DoesNotMatch("Test dünyasından merhaba", new Regex(@"[0-9]") );
+        }
+        
+        [TestMethod]
+        public void StartsWithTest()
+        {
+            StringAssert.Contains("Test dünyasından merhaba", "Test");
+        }
+        
+        [TestMethod]
+        public void EndsWithtTest()
+        {
+            StringAssert.Contains("Test dünyasından merhaba", "merhaba");
+        }
+    }
+}
